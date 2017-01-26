459e8e6cc8960585292d3b76b1089cf13ae88a2c
Ruslan Kuchumov <kuchumovri@gmail.com>
2017-01-26 15:49:22 +0300

    react init

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Monitor from '../../ui/pages/Monitor.jsx';
+
+export const renderRoutes = () => (
+  <Router history={browserHistory}>
+    <Route path="/monitor" component={Monitor} />
+  </Router>
+);
---
+++ imports/ui/pages/Monitor.jsx
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react';
+
+export default class Monitor extends Component {
+  render() {
+    return (
+      <div className="uk-container uk-container-expand">Hello World</div>
+    );
+  }
+}
