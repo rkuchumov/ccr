7d9bcdbed11138e83d916ae709dc910981c2d02f
Ruslan Kuchumov <kuchumovri@gmail.com>
2017-01-26 17:52:33 +0300

    added channels api

---
+++ imports/api/channels.js
@@ -0,0 +1,78 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+export const Channels = new Mongo.Collection('channels');
+
+Channels.schema = new SimpleSchema({
+  _id: {
+    type: String,
+    label: "Channel ID",
+    max: 32
+  },
+  title: {
+    type: String,
+    label: "Title",
+  },
+  location: {
+    type: String,
+    label: "Location",
+    optional: true,
+  },
+  language: {
+    type: String,
+    label: "Language",
+    optional: true,
+  },
+  description: {
+    type: String,
+    label: "Language",
+    optional: true,
+  },
+  website: {
+    type: String,
+    label: "Official website",
+    optional: true,
+  },
+  tags: {
+    type: [String],
+    label: "Tags",
+    optional: true,
+  },
+  createdAt: {
+    type: Date,
+  },
+  updatedAt: {
+    type: Date,
+  }
+});
+
+Channels.attachSchema(Channels.schema);
+
+if (Meteor.isServer) {
+  Meteor.publish('channels', function channelsPublication() {
+    return Channels.find();
+  });
+}
+
+Meteor.methods({
+  'channel.upsert'(channel) {
+    // TODO: replace with insert and update procedures
+    Channels.upsert(channel._id, {
+      title:        channel.title,
+      location:     channel.location,
+      language:     channel.language,
+      website:      channel.website,
+      tags:         channel.tags,
+      description:  channel.description,
+      createdAt:    new Date(),
+      updatedAt:    new Date(),
+    }, (error, result) => {
+      console.log("channel.upsert");
+      console.log("result", result);
+      console.log("error", error);
+      console.log(Channels.simpleSchema().namedContext().validationErrors());
+    });
+  },
+
+});
