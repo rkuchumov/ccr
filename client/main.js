9e4e4d05db87a5e27c3510b0dcd440e3ebd87963
Ruslan Kuchumov <kuchumovri@gmail.com>
2017-01-26 16:39:08 +0300

    uikit init & added .meteor/

--- client/main.js
+++ client/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes.js';
 
+import '../imports/ui/uikit/uikit.theme.less';
+
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('render-target'));
 });
