172cb3445a3b3611a9a58a1a58d339ab9bbb2606
Ruslan Kuchumov <kuchumovri@gmail.com>
2017-10-28 23:10:38 +0300

    remove meteor stuff

--- app/src/server/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-import '../imports/api/channels.js';
-import { Captions, Streaming } from '../imports/api/captions.js';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
