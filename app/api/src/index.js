04955eb942a7a552260c298bc1f01fe9694f781f
satyaim <thisissatyavrat@gmail.com>
2018-03-03 05:33:06 +0530

    Add Preloading Spinner
    Add Preloading Spinner
    
    Add Preloading Spinner

--- app/api/src/index.js
+++ app/api/src/index.js
@@ -2,7 +2,6 @@
 const logger = require('winston');
 const server = require('./server');
 const port = server.get('port');
-console.log("port :"+port);
 const handle = server.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
--- app/api/src/mongodb.js
+++ app/api/src/mongodb.js
@@ -3,7 +3,6 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = function () {
   const app = this;
   const config = app.get('mongodb');
-  console.log("config : "+ config);
   const promise = MongoClient.connect(config);
 
   app.set('mongoClient', promise);
